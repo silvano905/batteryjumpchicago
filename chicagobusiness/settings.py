b5ecd6766c7575022295ac001f7d6827eed4f65e
silvano valdez <silvanovaldez90@gmail.com>
2020-04-08 15:24:31 -0500

    ss

--- chicagobusiness/settings.py
+++ chicagobusiness/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ['MY_KEY_CHICAGO']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['business2020.pythonanywhere.com', '127.0.0.1']
+ALLOWED_HOSTS = ['business2020.pythonanywhere.com', '127.0.0.1','www.batteryjumpchicago.com']
 
 
 # Application definition
